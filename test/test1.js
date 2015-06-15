45cc369ac36b5b9c0a1bb2e2078c3d725718765a
testci-cinche <si@Simons-MacBook-Pro.local>
2015-06-15 11:30:55 +0100

    make test fail

--- test/test1.js
+++ test/test1.js
@@ -3,7 +3,7 @@ describe('Test Suite', function(){
   describe('TestCase1', function(){
     it('should always pass', function(){
       assert.equal(1, 1);
-      assert.equal(2, 2);
+      assert.equal(3, 2);
     })
   })
 })
